3a225716bab5d6af07ad792200899256ff8898bf
Roy Warungu <squadvlog44@gmail.com>
2025-03-24 01:13:32 +0300

    Added files via Upload

---
+++ myscript.js
@@ -0,0 +1,3 @@
+export const  visitCv = () => {
+    window.location.href = "https://docs.google.com/document/d/e/2PACX-1vTqqnyDLE0iGq5vAZcElSY5NNkRial7zFIPDhwbBzFTsc7hmvHq_CmOwyh3sUNFR2XaQ7MeqPsTCpez/pub";
+}
